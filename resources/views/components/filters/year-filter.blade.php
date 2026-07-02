1a3f618e830a46763197d4b9db3d96baf2a34e27
Kurniawan <kurni.aawann19@gmail.com>
2026-07-02 23:07:46 +0700

    feat: Add onchange event to month and year filters for automatic form submission

--- resources/views/components/filters/year-filter.blade.php
+++ resources/views/components/filters/year-filter.blade.php
@@ -3,6 +3,7 @@
 <div class="w-full lg:w-auto">
     <label for="{{ $name }}-select" class="sr-only">Pilih Tahun</label>
     <select name="{{ $name }}" id="{{ $name }}-select"
+        onchange="this.form.requestSubmit()"
         class="block w-full lg:w-32 rounded-lg border border-border-strong bg-surface-secondary p-3 text-sm text-text-input 
                focus:outline-none focus:border-primary focus:ring-2 focus:ring-primary-light"
         {{ $attributes }}>
